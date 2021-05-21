d84a2af5bb3f6bb669d3c562b4b556b2fc6e3844
aman189574 <33549036+aman189574@users.noreply.github.com>
2021-05-21 14:38:32 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -98,7 +98,7 @@ else
     echo .
     echo .
     install_deps
-    echo This Script Was Made By SpeedX > .update
+    echo This Script Was Made by Aman> .update
     echo 'Requirements Installed....'
     pause
 fi
